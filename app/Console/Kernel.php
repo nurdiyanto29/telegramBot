df7b3f22e06238b2a2280108caa7b0dfc1634044
xnr <nurdi014@gmail.com>
2024-01-08 20:37:11 +0700

    no message

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -34,7 +34,7 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('batasbayar:cron')->dailyAt('20:30');
+        $schedule->command('batasbayar:cron')->dailyAt('20:40');
 
     }
 
